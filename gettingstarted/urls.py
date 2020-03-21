28f76f0fce336feda314d3ce1928ea4764973ed0
Paul FLeming <pfleming@heroku.com>
2020-03-21 17:02:09 +0000

    Added polls app

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -16,6 +16,7 @@ import hello.views
 
 urlpatterns = [
     path("", hello.views.index, name="index"),
+    path('polls/', include('polls.urls')),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
